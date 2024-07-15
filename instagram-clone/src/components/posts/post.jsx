50b217760f048e8ec0dcfde2fba76300ccf43fc4
Aravindmt0 <aravind_mt@yatnam.com>
2024-07-15 15:24:23 +0530

    insta-clone-changes2

--- instagram-clone/src/components/posts/post.jsx
+++ instagram-clone/src/components/posts/post.jsx
@@ -1,3 +1,9 @@
+import styles from "./post.module.css";
 export default function Post() {
-    return <div></div>
+    return <div className={styles.post_block}>
+        <h3 className={styles.post_username}>username</h3>
+        <figure className={styles.post_profile}><img className={styles.post_image} src="" alt="" />
+        </figure>
+        <h4>username :caption</h4>
+    </div>
 }
