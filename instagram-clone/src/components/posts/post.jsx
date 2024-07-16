e4e86816ae95c2b6c1baed4e84f4f7f5f33f5411
Aravindmt0 <aravind_mt@yatnam.com>
2024-07-16 17:50:24 +0530

    insta-clone-changes4

--- instagram-clone/src/components/posts/post.jsx
+++ instagram-clone/src/components/posts/post.jsx
@@ -1,9 +1,15 @@
 import styles from "./post.module.css";
+import postimage from '../../assets/images/postimage.png'
+import Avatar from '@mui/material/Avatar';
 export default function Post() {
+    
     return <div className={styles.post_block}>
+        <div className={styles.post_top_block}>
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
         <h3 className={styles.post_username}>username</h3>
-        <figure className={styles.post_profile}><img className={styles.post_image} src="" alt="" />
+        </div>
+        <figure className={styles.post_profile}><img className={styles.post_image} src={postimage} alt="" />
         </figure>
-        <h4>username :caption</h4>
+        <h4 className={styles.username_caption}><strong>username</strong> :caption</h4>
     </div>
 }
