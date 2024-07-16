d1fa7f4d888b844dcde5841e49e747664ef7888c
Aravindmt0 <aravind_mt@yatnam.com>
2024-07-16 14:16:12 +0530

    insta-clone-changes3

--- instagram-clone/src/App.jsx
+++ instagram-clone/src/App.jsx
@@ -3,13 +3,14 @@ import Header from "./components/Header/Header";
 import Post from "./components/posts/post";
 import Container from "./components/Container";
 import "./App.module.css";
+import InnerContainer from "./components/InnerContainer";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <Container>
-        <Header />
+        <InnerContainer> <Header /></InnerContainer>
         <Post />
       </Container>
     </div>
