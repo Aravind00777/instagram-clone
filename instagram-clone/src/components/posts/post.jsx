1716aa658cd3c723a4bd7fcfccf217ac5c6ddae9
Aravindmt0 <aravind_mt@yatnam.com>
2024-07-16 18:22:45 +0530

    commit5

--- instagram-clone/src/components/posts/post.jsx
+++ instagram-clone/src/components/posts/post.jsx
@@ -5,7 +5,7 @@ export default function Post() {
     
     return <div className={styles.post_block}>
         <div className={styles.post_top_block}>
-        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <Avatar className={styles.post_avatar} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
         <h3 className={styles.post_username}>username</h3>
         </div>
         <figure className={styles.post_profile}><img className={styles.post_image} src={postimage} alt="" />
